7e01ede2d446104ebc6c92abbcda1bc680751437
clintjeez <clintjeez@gmail.com>
2021-01-04 10:58:50 +0100

    checkout-form

--- src/App.js
+++ src/App.js
@@ -10,17 +10,27 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems")
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [],
       size: "",
       sort: "",
     };
   }
+  // create order handler
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
+  };
   // removeFromCart function
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({
       cartItems: cartItems.filter((x) => x._id !== product._id),
     });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
   };
 
   // Add to cart function
@@ -39,6 +49,7 @@ class App extends React.Component {
     this.setState({
       cartItems,
     });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
 
   // Product sorting function
@@ -106,6 +117,7 @@ class App extends React.Component {
               <Cart
                 cartItems={this.state.cartItems}
                 removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
               />
             </div>
           </div>
